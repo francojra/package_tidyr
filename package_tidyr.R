c0a8125e6098f07bae56fb6cb2bf31f4a1e6270a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-03-11 20:44:05 -0300

    Add files via upload

---
+++ package_tidyr.R
@@ -0,0 +1,104 @@
+# Pacote tidyr - Tidyverse -----------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 11/03/2022 -------------------------------------------------------------------------------------------------------------------------
+# Fonte: Curso R ---------------------------------------------------------------------------------------------------------------------------
+
+# O pacote tidyr ---------------------------------------------------------------------------------------------------------------------------
+
+## Dentro do tidyverse, uma base tidy é uma base fácil de se trabalhar, isto é, fácil de 
+## se fazer manipulação de dados,  fácil de se criar visualizações, fácil de se ajustar 
+## modelos e por aí vai.
+
+## Na prática, uma base tidy é aquela que se encaixa bem no framework do tidyverse, 
+## pois os pacotes como o dplyr e o ggplot2 foram desenvolvidos para funcionar bem 
+## com bases tidy. E assim como esses pacotes motivaram o uso de bases tidy, 
+## o conceito tidy motiva o surgimento de novos frameworks, como o tidymodels para modelagem.
+
+## As duas propriedades mais importantes de uma base tidy são:
+
+## - cada coluna é uma variável;
+
+## - cada linha é uma observação.
+
+## Essa definição proporciona uma maneira consistente de se referir a variáveis 
+## (nomes de colunas) e observações (índices das linhas).
+
+## O pacote {tidyr} possui funções que nos ajudam a transformar uma base bagunçada 
+## em uma base tidy. Ou então, nos ajudam a bagunçar um pouquinho a nossa base 
+## quando isso nos ajudar a produzir o resultados que queremos.
+
+## Vamos ver aqui algumas de suas principais funções:
+
+## - separate() e unite(): para separar variáveis concatenadas em uma única coluna ou uni-las.
+
+## - pivot_wider() e pivot_longer(): para pivotar a base.
+
+## - nest() e unnest(): para criar list columns.
+
+## Como motivação para utilizar esssas funções, vamos utilizar a nossa boa e velha base imdb.
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+imdb <- readr::read_rds("imdb.rds")
+View(imdb)
+
+# separate() e unite() ---------------------------------------------------------------------------------------------------------------------
+
+## A função separate() separa duas ou mais variáveis que estão concatenadas em uma mesma coluna. 
+## A sintaxe da função está apresentada abaixo.
+
+dados %>% 
+  separate( 
+    col = coluna_velha, 
+    into = c("colunas", "novas"),
+    sep = "separador"
+  )
+
+## Como exemplo, vamos transformar a coluna generos da base IMDB em três colunas, cada uma 
+## com um dos gêneros do filme. Lembrando que os valores da coluna generos estão no seguinte 
+## formato:
+
+imdb %>% pull(generos) %>% head()
+
+## Veja que agora, temos 3 colunas de gênero. Filmes com menos de 3 gêneros recebem NA na 
+## coluna genero2 e/ou genero3. Os gêneros sobressalentes são descartados, assim como a
+## coluna generos original.
+
+imdb_1 <- imdb %>% 
+  separate( 
+    col = generos,
+    into = c("genero1", "genero2", "genero3"), 
+    sep = "\\|"
+  )
+
+View(imdb_1)
+
+## A função unite() realiza a operação inversa da função separate(). 
+## Ela concatena os valores de várias variáveis em uma única coluna. A sintaxe é a seguinte:
+
+dados %>% 
+  unite(
+    col = coluna_nova, 
+    colunas_para_juntar, 
+    sep = "separador" 
+  )
+
+## Como exemplo, vamos agora transformar as colunas ator1, ator2 e ator3 em uma única coluna 
+## atores. Lembrando que essas colunas estão no formato abaixo.
+
+imdb %>% select(starts_with("ator")) %>% head(3)
+
+## Veja que agora a coluna atores possui os 3 atores concatenados. Se a ordem das colunas 
+## ator1, ator2 e ator3 nos trazia a informação de protagonismo, essa informação passa a 
+## ficar implícita nesse novo formato. As 3 colunas originais são removidas da base resultante.
+
+imdb_2 <- imdb %>% 
+  unite(
+    col = "elenco",
+    starts_with("ator"), 
+    sep = " - "
+  ) 
+
+View(imdb_2)
